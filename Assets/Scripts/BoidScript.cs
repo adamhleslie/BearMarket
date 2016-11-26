73a680114be3899b983d3be5660db0f8a082cfca
LiShengShun <yellowjinn@gmail.com>
2016-11-26 17:32:56 -0600

    starting boid scripts

---
+++ Assets/Scripts/BoidScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoidScript : MonoBehaviour {
+
+  public static int[][] boidArr;
+  private int id;
+  public
+
+  // Use this for initialization
+  void Start () {
+    if (boidArr == null) {
+      boidArr = new int[][];
+    }
+    id = new Random().Next(1,10000);
+  }
+
+  // Update is called once per frame
+  void Update () {
+
+  }
+
+}
--- Assets/Scripts/InstantSpawnPoint.cs
+++ Assets/Scripts/InstantSpawnPoint.cs
@@ -5,45 +5,46 @@ using UnityEngine.Assertions;
 
 public class InstantSpawnPoint : MonoBehaviour {
 
-	// Set in the inspector as the thing that will be spawned
-	public GameObject blueprint;
+  // Set in the inspector as the thing that will be spawned
+  public GameObject blueprint;
 
-	public int numToSpawn;
+  public int numToSpawn;
 
-	// All set in editor (each should have a size of 2)
-	public float[] scaleRange;
-	public float[] xRange;
-	public float[] yRange;
-	public float[] zRange;
+  // All set in editor (each should have a size of 2)
+  public float[] scaleRange;
+  public float[] xRange;
+  public float[] yRange;
+  public float[] zRange;
 
-	// Use this for initialization
-	private void Start () 
-	{
+  // Use this for initialization
+  private void Start ()
+  {
 
-		Assert.IsNotNull(blueprint);
-		blueprint.SetActive(false);
-	}
-	
-	// Update is called once per frame
-	private void Update () 
-	{
-		while (numToSpawn > 0)
-		{
-			// Instantiate Template
-			GameObject spawnedObject = Instantiate(blueprint);
-			Transform spawnedTransf = spawnedObject.GetComponent<Transform>();
-			spawnedTransf.SetParent(GetComponent<Transform>(), false);
+    Assert.IsNotNull(blueprint);
+    blueprint.SetActive(false);
+  }
 
-			float scale = Random.Range(scaleRange[0], scaleRange[1]);
-			spawnedTransf.localScale = new Vector3(scale, scale, scale);
-			spawnedTransf.localPosition = new Vector3(Random.Range(xRange[0], xRange[1]), 
-													  Random.Range(yRange[0], yRange[1]),
-													  Random.Range(zRange[0], zRange[1]));
-			spawnedObject.SetActive(true);
-			spawnedTransf.SetParent(null);
+  // Update is called once per frame
+  private void Update ()
+  {
+    while (numToSpawn > 0)
+    {
+      // Instantiate Template
+      GameObject spawnedObject = Instantiate(blueprint);
+      Transform spawnedTransf = spawnedObject.GetComponent<Transform>();
+      spawnedTransf.SetParent(GetComponent<Transform>(), false);
 
-			numToSpawn--;
-		}
-		Destroy(gameObject);
-	}
+      float scale = Random.Range(scaleRange[0], scaleRange[1]);
+      spawnedTransf.localScale = new Vector3(scale, scale, scale);
+      spawnedTransf.localPosition = new Vector3(
+          Random.Range(xRange[0], xRange[1]),
+          Random.Range(yRange[0], yRange[1]),
+          Random.Range(zRange[0], zRange[1]));
+      spawnedObject.SetActive(true);
+      spawnedTransf.SetParent(null);
+
+      numToSpawn--;
+    }
+    Destroy(gameObject);
+  }
 }
