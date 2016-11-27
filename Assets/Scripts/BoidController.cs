4c9bbb706643275ec191f9d1da045cd3075ee0ba
Adam Leslie <adamhleslie@gmail.com>
2016-11-27 14:44:09 -0600

    Code cleanup, and working scene

--- Assets/Scripts/BoidController.cs
+++ Assets/Scripts/BoidController.cs
@@ -3,93 +3,85 @@ using System.Collections.Generic;
 
 public class BoidController : MonoBehaviour
 {
-  private Rigidbody body;
+	private Rigidbody body;
 
-  [SerializeField]
-    private float maxMagnitude;
+	[SerializeField]
+	private float maxMagnitude;
 
-  // Need to store local awareness of
-  // 1. Boids
-  [SerializeField]
-    public static Dictionary<int, Vector3> boidList;
-  public int id;
-  // 2. Obstacles
-  // 3. Predators
-  public Vector3 predLoc;
+	// Need to store local awareness of
+	// 1. Boids
+	private static Dictionary<int, Vector3> boidList;
+	private int id;
+	// 2. Obstacles
+	// 3. Predators
+	private Vector3 predLoc;
 
-  // Initialization
-  void Start ()
-  {
-    body = GetComponent<Rigidbody>();
+	// Initialization
+	void Start ()
+	{
+		body = GetComponent<Rigidbody>();
 
-    // Initialize the boidList if needed
-    if (boidList == null) {
-      boidList = new Dictionary<int, Vector3>();
-    }
-    // Generate new id for this boid
-    id = (int) Random.Range(1.0f,10000.0f);
-    // Add to boidList
-    boidList.Add(id, body.position);
-  }
+		// Initialize the boidList if needed
+		if (boidList == null)
+			boidList = new Dictionary<int, Vector3>();
 
-  // Update is called once per frame
-  void FixedUpdate ()
-  {
-    // Step 1: Update spatial awareness
-    boidList[id] = body.position;
+		// Generate new id for this boid
+		id = (int) Random.Range(1.0f,10000.0f);
+		// Add to boidList
+		boidList.Add(id, body.position);
+	}
 
-    // Step 2: Generate a container of acceleration vectors
-    List<Vector3> accelerations = new List<Vector3>();
-    Vector3 singleAcc = new Vector3();
-    foreach(KeyValuePair<int, Vector3> entry in boidList)
-    {
-      if (entry.Key != id) {
-        float dist = Vector3.Distance(entry.Value, body.position);
-        // If close, move away
-        if (dist < 3) {
-          singleAcc = body.position - entry.Value;
-          singleAcc.z = singleAcc.z * maxMagnitude;
-          singleAcc.y = singleAcc.y * maxMagnitude;
-          singleAcc.x = singleAcc.x * maxMagnitude;
-          accelerations.Add(singleAcc);
-        }
-        // If far, move closer
-        else if (dist > 4) {
-          singleAcc = entry.Value - body.position;
-          singleAcc.z = singleAcc.z * maxMagnitude;
-          singleAcc.y = singleAcc.y * maxMagnitude;
-          singleAcc.x = singleAcc.x * maxMagnitude;
-          accelerations.Add(singleAcc);
-        }
-      }
-    }
+	void FixedUpdate ()
+	{
+		// Step 1: Update spatial awareness
+		boidList[id] = body.position;
 
-    // Step 3: Accumulate acceleration vectors
-    Vector3 output = AccumulateAccelerations(accelerations);
+		// Step 2: Generate a container of acceleration vectors
+		List<Vector3> accelerations = new List<Vector3>();
+		foreach (KeyValuePair<int, Vector3> entry in boidList)
+		{
+			if (entry.Key != id) 
+			{
+				float dist = Vector3.Distance(entry.Value, body.position);
+				if (dist < 5) 
+				{
+					// If close, move away
+					accelerations.Add((body.position - entry.Value) * 20);
+				}
+				else if (dist < 15) 
+				{
+					// If far, move closer
+					accelerations.Add((entry.Value - body.position) * 2);
+				}
+			}
+		}
 
-    // Step 4: Apply acceleration vectors to boid
-    body.AddForce(output);
-  }
+		// Step 3: Accumulate acceleration vectors
+		Vector3 output = AccumulateAccelerations(accelerations);
 
-  private Vector3 AccumulateAccelerations (ICollection<Vector3> accelerations)
-  {
-    float totalMagnitude = 0;
-    Vector3 result = Vector3.zero;
+		// Step 4: Apply acceleration vectors to boid
+		body.AddForce(output);
+	}
 
-    foreach (Vector3 acc in accelerations)
-    {
-      totalMagnitude += acc.magnitude;
-      if (totalMagnitude >= maxMagnitude)
-      {
-        // Scale the acceleration down when its entire magnitude not used
-        float scale = 1 - ((totalMagnitude - maxMagnitude) / acc.magnitude);
-        result += scale * acc;
-        break;
-      }
-      else
-        result += acc;
-    }
+	private Vector3 AccumulateAccelerations (ICollection<Vector3> accelerations)
+	{
+		float totalMagnitude = 0;
+		Vector3 result = Vector3.zero;
 
-    return result;
-  }
+		foreach (Vector3 acc in accelerations)
+		{
+			totalMagnitude += acc.magnitude;
+			if (totalMagnitude >= maxMagnitude)
+			{
+				// Scale the acceleration down when its entire magnitude not used
+				float scale = 1 - ((totalMagnitude - maxMagnitude) / acc.magnitude);
+				result += scale * acc;
+				break;
+			}
+			else
+				result += acc;
+		}
+
+		return result;
+	}
 }
