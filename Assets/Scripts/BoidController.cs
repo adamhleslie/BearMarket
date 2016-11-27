732df6a3c9cef833a83a1b2b707732d57ec3c86d
LiShengShun <yellowjinn@gmail.com>
2016-11-26 23:05:10 -0600

    moves, but the accelleration collection is not there yet

--- Assets/Scripts/BoidController.cs
+++ Assets/Scripts/BoidController.cs
@@ -1,58 +1,96 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 
-public class BoidController : MonoBehaviour 
+public class BoidController : MonoBehaviour
 {
-	private Rigidbody body;
+  private Rigidbody body;
 
-	[SerializeField]
-	private float maxMagnitude;
+  [SerializeField]
+    private float maxMagnitude;
 
-	// Need to store local awareness of
-	// 1. Boids
-	// 2. Obstacles
-	// 3. Predators
+  // Need to store local awareness of
+  // 1. Boids
+  [SerializeField]
+    public static Dictionary<int, Vector3> boidList;
+  public int id;
+  // 2. Obstacles
+  // 3. Predators
+  public Vector3 predLoc;
 
-	// Initialization
-	void Start ()
-	{
-		body = GetComponent<Rigidbody>();
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate ()
-	{
-		// Step 1: Update spatial awareness
+  // Initialization
+  void Start ()
+  {
+    body = GetComponent<Rigidbody>();
 
-		// Step 2: Generate a container of acceleration vectors
+    // Initialize the boidList if needed
+    if (boidList == null) {
+      boidList = new Dictionary<int, Vector3>();
+    }
+    // Generate new id for this boid
+    id = (int) Random.Range(1.0f,10000.0f);
+    // Add to boidList
+    boidList.Add(id, body.position);
+  }
 
+  // Update is called once per frame
+  void FixedUpdate ()
+  {
+    // Step 1: Update spatial awareness
+    boidList[id] = body.position;
 
-		// Step 3: Accumulate acceleration vectors
+    // Step 2: Generate a container of acceleration vectors
+    //ICollection<Vector3> accelerations = new Collection<Vector3>();
+    Vector3 singleAcc = new Vector3();
+    foreach(KeyValuePair<int, Vector3> entry in boidList)
+    {
+      if (entry.Key != id) {
+        float dist = Vector3.Distance(entry.Value, body.position);
+        // If close, move away
+        if (dist < 3) {
+          singleAcc = body.position - entry.Value;
+          singleAcc.z = singleAcc.z * maxMagnitude;
+          singleAcc.y = singleAcc.y * maxMagnitude;
+          singleAcc.x = singleAcc.x * maxMagnitude;
+          //accelerations.Add(singleAcc);
+        }
+        // If far, move closer
+        else if (dist > 4) {
+          singleAcc = entry.Value - body.position;
+          singleAcc.z = singleAcc.z * maxMagnitude;
+          singleAcc.y = singleAcc.y * maxMagnitude;
+          singleAcc.x = singleAcc.x * maxMagnitude;
+          //accelerations.Add(singleAcc);
+        }
+      }
+    }
 
+    // Step 3: Accumulate acceleration vectors
+    //Vector3 output = AccumulateAccelerations(accelerations);
+    Vector3 output = Vector3.down;
 
+    // Step 4: Apply acceleration vectors to boid
+    body.AddForce(output);
+  }
 
-		// Step 4: Apply acceleration vectors to boid
-	}
+  private Vector3 AccumulateAccelerations (ICollection<Vector3> accelerations)
+  {
+    float totalMagnitude = 0;
+    Vector3 result = Vector3.zero;
 
-	private Vector3 AccumulateAccelerations (ICollection<Vector3> accelerations)
-	{
-		float totalMagnitude = 0;
-		Vector3 result = Vector3.zero;
+    foreach (Vector3 acc in accelerations)
+    {
+      totalMagnitude += acc.magnitude;
+      if (totalMagnitude >= maxMagnitude)
+      {
+        // Scale the acceleration down when its entire magnitude not used
+        float scale = 1 - ((totalMagnitude - maxMagnitude) / acc.magnitude);
+        result += scale * acc;
+        break;
+      }
+      else
+        result += acc;
+    }
 
-		foreach (Vector3 acc in accelerations)
-		{
-			totalMagnitude += acc.magnitude;
-			if (totalMagnitude >= maxMagnitude)
-			{
-				// Scale the acceleration down when its entire magnitude not used
-				float scale = 1 - ((totalMagnitude - maxMagnitude) / acc.magnitude);
-				result += scale * acc;
-				break;
-			}
-			else
-				result += acc;
-		}
-
-		return result;
-	}
+    return result;
+  }
 }
