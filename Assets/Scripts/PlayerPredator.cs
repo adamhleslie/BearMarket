9ca0ba8026d38b21c2c2f1b57b51b4a7c14f55c1
Adam Leslie <adamhleslie@gmail.com>
2016-12-05 09:49:46 -0600

    Updates!

--- Assets/Scripts/PlayerPredator.cs
+++ Assets/Scripts/PlayerPredator.cs
@@ -1,10 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 
-public class PlayerBoid : MonoBehaviour 
-{
+public class PlayerPredator : MonoBehaviour {
+
 	public Rigidbody body;
-	public bool attractionEnabled;
 
 	[SerializeField]
 	private float horizSpeed;
@@ -13,22 +12,16 @@ public class PlayerBoid : MonoBehaviour
 	private float vertSpeed;
 
 	// Use this for initialization
-	void Start () 
+	void Start ()
 	{
 		body = GetComponent<Rigidbody>();
-		if (BoidController.boidList == null)
-			BoidController.boidList = new List<Rigidbody>();
-		BoidController.boidList.Add(body);
-		BoidController.player = this;
 
-		attractionEnabled = false;
+		BoidController.predPlayers.Add(this);
 	}
-	
-	void FixedUpdate () 
+
+	void FixedUpdate ()
 	{
 		body.velocity = new Vector3(Input.GetAxis("Horizontal") * horizSpeed, 0, Input.GetAxis("Vertical") * vertSpeed);
 		body.rotation = Quaternion.LookRotation(body.velocity);
-
-		attractionEnabled = Input.GetAxis("Fire1") != 0;
 	}
 }
