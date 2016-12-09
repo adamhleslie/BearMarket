c2ccdbd151952e782fcd9eb448a7acc3aed0f3a9
LiShengShun <yellowjinn@gmail.com>
2016-12-09 11:42:47 -0600

    menu buttons now link to everything

---
+++ Assets/Scripts/SelectGame.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SelectGame : MonoBehaviour {
+
+        public Button singleColi;
+        public Button multiColi;
+        public Button singleLogo;
+        public Button cancel;
+
+	// Use this for initialization
+	void Start () {
+                singleColi.onClick.AddListener(StartSingleColi);
+                multiColi.onClick.AddListener(StartMultiColi);
+                singleLogo.onClick.AddListener(StartSingleLogo);
+                singleLogo.onClick.AddListener(CancelGame);
+	}
+
+        void StartSingleLogo () {
+          SceneManager.LoadScene("Logo");
+        }
+
+        void StartMultiColi() {
+          SceneManager.LoadScene("ColiseumMulti");
+        }
+
+        void StartSingleColi() {
+          SceneManager.LoadScene("Coliseum");
+        }
+
+        void CancelGame() {
+          SceneManager.LoadScene("Main Menu");
+        }
+
+}
--- Assets/Scripts/StartGame.cs
+++ Assets/Scripts/StartGame.cs
@@ -6,13 +6,76 @@ using UnityEngine.SceneManagement;
 public class StartGame : MonoBehaviour {
 
 	public Button startGame;
+        public Button options;
+        public Button quit;
+        public Text title;
+
+	public Button singleColi;
+        public Button multiColi;
+        public Button singleLogo;
+        public Button cancel;
+
 	// Use this for initialization
 	void Start () {
-		Button btn = startGame.GetComponent<Button>();
-		btn.onClick.AddListener(TaskOnClick);
-	}
-	
+          startGame.onClick.AddListener(TaskOnClick);
+
+          singleLogo.gameObject.SetActive(false);
+          singleColi.gameObject.SetActive(false);
+          multiColi.gameObject.SetActive(false);
+          cancel.gameObject.SetActive(false);
+
+          singleLogo.onClick.AddListener(StartSingleLogo);
+          singleColi.onClick.AddListener(StartSingleColi);
+          multiColi.onClick.AddListener(StartMultiColi);
+          cancel.onClick.AddListener(CancelGame);
+        }
+
+        void StartSingleLogo () {
+          CloseUI();
+          SceneManager.LoadScene("Logo");
+        }
+
+        void StartMultiColi() {
+          CloseUI();
+          SceneManager.LoadScene("ColiseumMulti");
+        }
+
+        void StartSingleColi() {
+          CloseUI();
+          SceneManager.LoadScene("Coliseum");
+        }
+
+        void CancelGame() {
+          startGame.gameObject.SetActive(true);
+          options.gameObject.SetActive(true);
+          quit.gameObject.SetActive(true);
+
+          singleLogo.gameObject.SetActive(false);
+          singleColi.gameObject.SetActive(false);
+          multiColi.gameObject.SetActive(false);
+          cancel.gameObject.SetActive(false);
+        }
+
 	void TaskOnClick () {
-		SceneManager.LoadScene ("Coliseum");
+          startGame.gameObject.SetActive(false);
+          options.gameObject.SetActive(false);
+          quit.gameObject.SetActive(false);
+
+          singleLogo.gameObject.SetActive(true);
+          singleColi.gameObject.SetActive(true);
+          multiColi.gameObject.SetActive(true);
+          cancel.gameObject.SetActive(true);
 	}
+
+        void CloseUI() {
+          title.gameObject.SetActive(false);
+          singleLogo.gameObject.SetActive(false);
+          singleColi.gameObject.SetActive(false);
+          multiColi.gameObject.SetActive(false);
+          cancel.gameObject.SetActive(false);
+
+          startGame.gameObject.SetActive(false);
+          options.gameObject.SetActive(false);
+          quit.gameObject.SetActive(false);
+        }
 }
