40339ee91a60869b272c69bbec7d89144f8b52c1
adamhleslie <adamhleslie@gmail.com>
2018-09-23 14:57:54 -0500

    Unpushed files, unsure of changes

---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class jukebox_script : MonoBehaviour 
+{
+	[System.NonSerialized]
+	public int colMultiBoidWins;
+
+	[System.NonSerialized]
+	public int colMultiBearWins;
+
+	[System.NonSerialized]
+	public 
+
+	[System.NonSerialized]
+	public 
+
+	private static bool firstRun = true;
+
+	void Awake ()
+	{
+		if (firstRun)
+			firstRun = false;
+		else
+			Destroy(gameObject);
+	}
+}
